00af3cdfb3226426ecb31459d3eaef893e5fa915
samad86 <mads198644@gmail.com>
2021-02-23 13:13:36 +0300

    refactor: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let config = Object.fromEntries(bracketsConfig);
+    let stack = [];
+
+    for (let char of str) {
+        if (stack.length && char === config[stack.slice(-1)]) stack.pop();
+        else if (char in config) stack.push(char);
+        else return false;
+    }
+
+    return stack.length === 0;
+};
